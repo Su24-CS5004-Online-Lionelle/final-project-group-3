7446668a5efc55065c31efc6112d76a263d02c3f
abegailsantos <aegailsantos.1819@gmail.com>
2024-08-05 22:49:52 -0400

    add: input validator class

---
+++ src/main/java/jobplanner/controller/InputValidator.java
@@ -0,0 +1,36 @@
+package jobplanner.controller;
+
+import jobplanner.model.formatters.Formats;
+
+public class InputValidator {
+
+    /**
+     * Validates the file extension based on the desired format.
+     *
+     * @param filename the name of the file to check
+     * @param format   the expected format
+     * @return true if the file extension matches the format, false otherwise
+     */
+    public static boolean isValidExtension(String filename, Formats format) {
+        String fileExtension = getFileExtension(filename);
+        switch (format) {
+            case CSV:
+                return "csv".equalsIgnoreCase(fileExtension);
+            case PRETTY:
+                return "txt".equalsIgnoreCase(fileExtension);
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Extracts the file extension from the filename.
+     *
+     * @param filename the name of the file
+     * @return the file extension
+     */
+    private static String getFileExtension(String filename) {
+        int dotIndex = filename.lastIndexOf('.');
+        return (dotIndex == -1) ? "" : filename.substring(dotIndex + 1);
+    }
+}
